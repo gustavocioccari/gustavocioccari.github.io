a8ed5afd4b23e9ac538409edb7c2fc7ef394b8a1
gustavocioccari <gustavocioccari@gmail.com>
2020-05-04 15:17:58 -0300

    Updates

---
+++ precache-manifest.9000a3ae68b5d4e515702f222320cb11.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129dd2ab25340675a96e733c9b3cf218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ccc46f9256cf9295eac",
+    "url": "/static/css/2.75ec312b.chunk.css"
+  },
+  {
+    "revision": "25745aff54253e2773ad",
+    "url": "/static/css/main.ca1fd221.chunk.css"
+  },
+  {
+    "revision": "9ccc46f9256cf9295eac",
+    "url": "/static/js/2.d4ec7632.chunk.js"
+  },
+  {
+    "revision": "f05a9da55f8862af6e537620307b99cd",
+    "url": "/static/js/2.d4ec7632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25745aff54253e2773ad",
+    "url": "/static/js/main.1798bbd7.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "fdbed7f114bab62e534e4e51b1d978a0",
+    "url": "/static/media/512x512.fdbed7f1.png"
+  },
+  {
+    "revision": "80fca5f2a6043bda9d84e48e04ea33ea",
+    "url": "/static/media/aa.80fca5f2.jpg"
+  }
+]);
